6b625541e9c298b91e1f559a9d17487d87329d5c
Woad47 <wooad47@gmail.com>
2024-03-05 11:38:38 -0400

    DB initialization updated to create users table

--- src/database/database.sql
+++ src/database/database.sql
@@ -1,4 +1,5 @@
-CREATE TABLE IF NOT EXISTS items ( id SERIAL PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS items ( 
+id SERIAL PRIMARY KEY, 
 product_name VARCHAR(255) NOT NULL, 
 product_price NUMERIC NOT NULL, 
 category VARCHAR(40) NOT NULL, 
@@ -15,3 +16,9 @@ VALUES (
 'https://m.media-amazon.com/images/I/61tJAIhYFYL._SX466_.jpg',
 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente? Illum suscipit, explicabo qui consectetur corrupti reprehenderit repellendus perspiciatis ab at dicta obcaecati facere excepturi? Dicta deserunt totam alias magnam. Optio numquam ut explicabo, provident repellat voluptatibus.')
 ON CONFLICT (product_id) DO NOTHING;
+
+CREATE TABLE IF NOT EXISTS users ( 
+id SERIAL PRIMARY KEY, 
+email VARCHAR(320) NOT NULL UNIQUE, 
+password VARCHAR(60) NOT NULL
+role CHAR NOT NULL DEFAULT 'u');
