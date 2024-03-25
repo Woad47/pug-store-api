a92cbbee5e1c7b3a1870dbbcc9b513073739494a
Woad47 <wooad47@gmail.com>
2024-03-25 09:59:40 -0400

    Shortened lorem ipsum text for items descriptions

--- src/database/database.sql
+++ src/database/database.sql
@@ -16,7 +16,7 @@ VALUES (
 'Food',
 'CF01',
 'https://m.media-amazon.com/images/I/61tJAIhYFYL._SX466_.jpg',
-'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente? Illum suscipit, explicabo qui consectetur corrupti reprehenderit repellendus perspiciatis ab at dicta obcaecati facere excepturi? Dicta deserunt totam alias magnam. Optio numquam ut explicabo, provident repellat voluptatibus.')
+'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente?')
 ON CONFLICT (product_id) DO NOTHING;
 
 INSERT INTO items (product_name, product_price, category, product_id, product_img, product_description)
@@ -26,7 +26,7 @@ VALUES (
 'Food',
 'DF01',
 'https://happydog_en_sb.cstatic.io/440x440/f/69110/480x480/c90b13149a/hd-vet-new-product-slider-packshots-sensible-11kg-300g.png',
-'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente? Illum suscipit, explicabo qui consectetur corrupti reprehenderit repellendus perspiciatis ab at dicta obcaecati facere excepturi? Dicta deserunt totam alias magnam. Optio numquam ut explicabo, provident repellat voluptatibus.')
+'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente?')
 ON CONFLICT (product_id) DO NOTHING;
 
 INSERT INTO items (product_name, product_price, category, product_id, product_img, product_description)
@@ -36,7 +36,7 @@ VALUES (
 'Toys',
 'T01',
 'https://store.titosvodka.com/cdn/shop/products/Titos_Rubber_Ball_Toy.jpg?v=1575646728',
-'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente? Illum suscipit, explicabo qui consectetur corrupti reprehenderit repellendus perspiciatis ab at dicta obcaecati facere excepturi? Dicta deserunt totam alias magnam. Optio numquam ut explicabo, provident repellat voluptatibus.')
+'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente?')
 ON CONFLICT (product_id) DO NOTHING;
 
 INSERT INTO items (product_name, product_price, category, product_id, product_img, product_description)
@@ -46,7 +46,7 @@ VALUES (
 'Beds',
 'DB01',
 'https://ralphand.co/cdn/shop/products/Balmoral-front-2.jpg?v=1691521053',
-'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente? Illum suscipit, explicabo qui consectetur corrupti reprehenderit repellendus perspiciatis ab at dicta obcaecati facere excepturi? Dicta deserunt totam alias magnam. Optio numquam ut explicabo, provident repellat voluptatibus.')
+'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente?')
 ON CONFLICT (product_id) DO NOTHING;
 
 INSERT INTO items (product_name, product_price, category, product_id, product_img, product_description)
@@ -56,7 +56,7 @@ VALUES (
 'Food',
 'CF02',
 'https://www.purina.com/sites/default/files/products/indoor-salmon-dry-cat-food.png',
-'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente? Illum suscipit, explicabo qui consectetur corrupti reprehenderit repellendus perspiciatis ab at dicta obcaecati facere excepturi? Dicta deserunt totam alias magnam. Optio numquam ut explicabo, provident repellat voluptatibus.')
+'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente?')
 ON CONFLICT (product_id) DO NOTHING;
 
 INSERT INTO items (product_name, product_price, category, product_id, product_img, product_description)
@@ -66,7 +66,7 @@ VALUES (
 'Food',
 'CF03',
 'https://www.purina-arabia.com/sites/default/files/2022-07/CAT%20CHOW%20ADULT%20Salmon%26Tuna%206x1.5kg%20N2%20XE.jpg',
-'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente? Illum suscipit, explicabo qui consectetur corrupti reprehenderit repellendus perspiciatis ab at dicta obcaecati facere excepturi? Dicta deserunt totam alias magnam. Optio numquam ut explicabo, provident repellat voluptatibus.')
+'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente?')
 ON CONFLICT (product_id) DO NOTHING;
 
 INSERT INTO items (product_name, product_price, category, product_id, product_img, product_description)
@@ -76,7 +76,7 @@ VALUES (
 'Food',
 'CF04',
 'https://nulo.com/hubfs/Website%20Migration%202022/Product%20Category%20Images/high-meat-kibble__cat.jpg',
-'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente? Illum suscipit, explicabo qui consectetur corrupti reprehenderit repellendus perspiciatis ab at dicta obcaecati facere excepturi? Dicta deserunt totam alias magnam. Optio numquam ut explicabo, provident repellat voluptatibus.')
+'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente?')
 ON CONFLICT (product_id) DO NOTHING;
 
 INSERT INTO items (product_name, product_price, category, product_id, product_img, product_description)
@@ -86,7 +86,7 @@ VALUES (
 'Toys',
 'T02',
 'https://images.thdstatic.com/productImages/f7cff863-7bbc-4ba1-be27-37f4ceb90a20/svn/cat-toys-aws-lkw9-3774-64_1000.jpg',
-'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente? Illum suscipit, explicabo qui consectetur corrupti reprehenderit repellendus perspiciatis ab at dicta obcaecati facere excepturi? Dicta deserunt totam alias magnam. Optio numquam ut explicabo, provident repellat voluptatibus.')
+'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente?')
 ON CONFLICT (product_id) DO NOTHING;
 
 INSERT INTO items (product_name, product_price, category, product_id, product_img, product_description)
@@ -96,7 +96,7 @@ VALUES (
 'Clothing',
 'DC01',
 'https://i.ebayimg.com/images/g/UoUAAOSwpUtgxSwV/s-l1600.jpg',
-'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente? Illum suscipit, explicabo qui consectetur corrupti reprehenderit repellendus perspiciatis ab at dicta obcaecati facere excepturi? Dicta deserunt totam alias magnam. Optio numquam ut explicabo, provident repellat voluptatibus.')
+'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente?')
 ON CONFLICT (product_id) DO NOTHING;
 
 INSERT INTO items (product_name, product_price, category, product_id, product_img, product_description)
@@ -106,7 +106,7 @@ VALUES (
 'Clothing',
 'DC02',
 'https://www.homesalive.ca/media/catalog/product/f/r/front_range_collar_600_x_600_.jpg',
-'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente? Illum suscipit, explicabo qui consectetur corrupti reprehenderit repellendus perspiciatis ab at dicta obcaecati facere excepturi? Dicta deserunt totam alias magnam. Optio numquam ut explicabo, provident repellat voluptatibus.')
+'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente?')
 ON CONFLICT (product_id) DO NOTHING;
 
 INSERT INTO items (product_name, product_price, category, product_id, product_img, product_description)
@@ -116,7 +116,7 @@ VALUES (
 'Clothing',
 'DA01',
 'https://speedog.com/12957-thickbox_default/arnes-perro-dog-copenhagen-comfort-walk-air.jpg',
-'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente? Illum suscipit, explicabo qui consectetur corrupti reprehenderit repellendus perspiciatis ab at dicta obcaecati facere excepturi? Dicta deserunt totam alias magnam. Optio numquam ut explicabo, provident repellat voluptatibus.')
+'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente?')
 ON CONFLICT (product_id) DO NOTHING;
 
 INSERT INTO items (product_name, product_price, category, product_id, product_img, product_description)
@@ -126,7 +126,7 @@ VALUES (
 'Clothing',
 'DS01',
 'https://m.media-amazon.com/images/I/91O957C5jKL.jpg',
-'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente? Illum suscipit, explicabo qui consectetur corrupti reprehenderit repellendus perspiciatis ab at dicta obcaecati facere excepturi? Dicta deserunt totam alias magnam. Optio numquam ut explicabo, provident repellat voluptatibus.')
+'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente?')
 ON CONFLICT (product_id) DO NOTHING;
 
 /*Creating the table users if it doesn't exists*/
@@ -138,3 +138,4 @@ role CHAR NOT NULL DEFAULT 'u');
 
 UPDATE items SET product_img = 'https://store.titosvodka.com/cdn/shop/products/Titos_Rubber_Ball_Toy.jpg?v=1575646728', WHERE id = 3;
 UPDATE items SET product_img = 'https://ralphand.co/cdn/shop/products/Balmoral-front-2.jpg?v=1691521053', WHERE id = 4;
+UPDATE items SET product_description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente?'
